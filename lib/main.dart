ac3a822be11c147b87b1d4c5ce0933aafc8c35dc
Engincan Çiçek <mstrengincan22tr@gmail.com>
2024-05-21 06:27:48 +0300

    Debug Banner Gone

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Not Defteri',
+      debugShowCheckedModeBanner: false, // debug banner closed
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
